829fb35ac031a92a32857d5dc844683a5d5ca05a
Rahul Yadav <rahulyadav142857@gmail.com>
2024-03-22 15:47:30 +0530

    Created a server which serves with Hello Word message

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, App, HttpServer, Responder};
+
+#[get("/")]
+async fn hello() -> impl Responder {
+    "Hello, world!"
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new().service(hello)
+    })
+        .bind("127.0.0.1:8080")?
+        .run()
+        .await
 }
