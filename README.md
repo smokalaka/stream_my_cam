c4e8d428f49b7bf37e9aeca2d6e7063fd1c16fea
Rahul Yadav <rahulyadav142857@gmail.com>
2024-03-23 15:33:19 +0530

    Add capability to create a video streaming server from camera

---
+++ README.md
@@ -0,0 +1,5 @@
+This is a Rust based application server which streams webcam video.
+
+For issues with opencv installation and setup refer this issue:
+https://github.com/twistedfall/opencv-rust/issues/118
+
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,64 @@
-use actix_web::{get, App, HttpServer, Responder};
+use opencv::{
+    core::{Mat, Vector}, imgcodecs, prelude::*, videoio,
+};
 
-#[get("/")]
-async fn hello() -> impl Responder {
-    "Hello, world!"
+use std::net::TcpListener;
+use std::io::Write;
+use std::fmt::Display;
+
+fn main() {
+    let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
+    println!("Server listening on port 8080");
+
+    let mut cam = videoio::VideoCapture::new(0, videoio::CAP_ANY).expect("Failed to get video capture");
+    let mut frame = Mat::default();
+    let mut buf = Vector::new();
+
+    loop {
+        let (mut stream, requester_address) = listener.accept().expect("Failed to accept connection");
+
+        println!("Requester address: {}", requester_address);
+
+        cam.read(&mut frame).expect("Failed to capture frame");
+        buf.clear();
+        let _ = imgcodecs::imencode(".jpg", &frame, &mut buf, &Vector::new());
+
+        let response = format!(
+            "HTTP/1.1 200 OK\r\nContent-Type: multipart/x-mixed-replace; boundary=frame\r\n\r\n"
+        );
+
+        stream.write_all(response.as_bytes()).unwrap();
+
+        loop {
+            cam.read(&mut frame).expect("Failed to capture frame");
+            buf.clear();
+            let _ = imgcodecs::imencode(".jpg", &frame, &mut buf, &Vector::new());
+
+            let image_data = format!(
+                "--frame\r\nContent-Type: image/jpeg\r\nContent-Length: {}\r\n\r\n",
+                buf.len()
+            );
+
+            if let Err(err) = stream.write_all(image_data.as_bytes()) {
+                print_error(err);
+                break;
+            }
+            if let Err(err) = stream.write_all(buf.as_slice()) {
+                print_error(err);
+                break;
+            }
+            if let Err(err) = stream.write_all(b"\r\n") {
+                print_error(err);
+                break;
+            }
+            if let Err(err) = stream.flush() {
+                print_error(err);
+                break;
+            }
+        }
+    }
 }
 
-#[actix_web::main]
-async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new().service(hello)
-    })
-        .bind("127.0.0.1:8080")?
-        .run()
-        .await
+fn print_error<T: Display>(error: T) {
+    println!("Error sending data, probably connection terminated. Error: {}", error);
 }
