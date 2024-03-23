d63c59921c7df0398d1a075c4d2a8503678bcdb6
Rahul Yadav <rahulyadav142857@gmail.com>
2024-03-24 00:59:46 +0530

    Use Rust channels and concurrency to make sending data more efficient

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,46 @@
 use opencv::{
-    core::{Mat, Vector}, imgcodecs, prelude::*, videoio,
+    core::{Mat, Vector},
+    imgcodecs,
+    prelude::*,
+    videoio,
 };
-
 use std::net::TcpListener;
 use std::io::Write;
 use std::fmt::Display;
-use std::sync::{Arc, Mutex};
+use std::sync::Arc;
+use std::sync::Mutex;
 use std::thread;
+use crossbeam::channel;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:8080").expect("Failed to bind to port 8080");
     println!("Server listening on port 8080");
 
-    let cam = Arc::new(Mutex::new(videoio::VideoCapture::new(0, videoio::CAP_ANY).expect("Failed to get video capture")));
+    let (frame_sender, frame_receiver) = channel::bounded(10);
+    let cam = videoio::VideoCapture::new(0, videoio::CAP_ANY).expect("Failed to get video capture");
+
+    thread::spawn(move || {
+        let cam_mutex = Arc::new(Mutex::new(cam));
+        capture_frames(cam_mutex, frame_sender);
+    });
 
     for stream_result in listener.incoming() {
         match stream_result {
             Ok(mut stream) => {
-                let cam = cam.clone();
-                let _ = thread::spawn(move || {
-                    handle_client(&mut stream, &cam);
+                let response = format!(
+                    "HTTP/1.1 200 OK\r\nContent-Type: multipart/x-mixed-replace; boundary=frame\r\n\r\n"
+                );
+                if let Err(err) = stream.write_all(response.as_bytes()) {
+                    print_error(err);
+                    println!("Was not able to handle client");
+                    return;
+                }
+
+                let frame_receiver_clone = frame_receiver.clone();
+
+                thread::spawn(move || {
+                    let mut stream = stream;
+                    handle_client(&mut stream, frame_receiver_clone);
                 });
             }
             Err(err) => {
@@ -29,58 +50,62 @@ fn main() {
     }
 }
 
-fn handle_client(stream: &mut std::net::TcpStream, cam: &Arc<Mutex<videoio::VideoCapture>>) {
-    println!("New client connected");
+fn capture_frames(cam: Arc<Mutex<videoio::VideoCapture>>, frame_sender: channel::Sender<Vec<u8>>) {
+    let mut frame = Mat::default();
+    let mut buf = Vector::new();
+
+    let mut cam = cam.lock().unwrap();
 
     loop {
-        let response = format!(
-            "HTTP/1.1 200 OK\r\nContent-Type: multipart/x-mixed-replace; boundary=frame\r\n\r\n"
-        );
-        if let Err(err) = stream.write_all(response.as_bytes()) {
+        if let Err(err) = cam.read(&mut frame) {
             print_error(err);
             break;
         }
 
-        loop {
-            let mut cam = cam.lock().expect("Failed to lock VideoCapture");
+        if let Err(err) = imgcodecs::imencode(".jpg", &frame, &mut buf, &Vector::new()) {
+            print_error(err);
+            break;
+        }
 
-            let mut frame = Mat::default();
-            let mut buf = Vector::new();
+        if let Err(err) = frame_sender.send(buf.to_vec()) {
+            println!("Error sending frame: {}", err);
+            break;
+        }
+    }
+}
 
-            if let Err(err) = cam.read(&mut frame) {
-                print_error(err);
+fn handle_client(stream: &mut std::net::TcpStream, frame_receiver: channel::Receiver<Vec<u8>>) {
+    println!("New client connected");
+
+    loop {
+        let frame_buffer = match frame_receiver.recv() {
+            Ok(data) => data,
+            Err(err) => {
+                println!("Error receiving frame: {}", err);
                 break;
             }
+        };
 
-            buf.clear();
-            if let Err(err) = imgcodecs::imencode(".jpg", &frame, &mut buf, &Vector::new()) {
-                print_error(err);
-                break;
-            }
+        let image_data = format!(
+            "--frame\r\nContent-Type: image/jpeg\r\nContent-Length: {}\r\n\r\n",
+            frame_buffer.len()
+        );
 
-            let image_data = format!(
-                "--frame\r\nContent-Type: image/jpeg\r\nContent-Length: {}\r\n\r\n",
-                buf.len()
-            );
-
-            if let Err(err) = stream.write_all(image_data.as_bytes()) {
-                print_error(err);
-                break;
-            }
-            if let Err(err) = stream.write_all(buf.as_slice()) {
-                print_error(err);
-                break;
-            }
-            if let Err(err) = stream.write_all(b"\r\n") {
-                print_error(err);
-                break;
-            }
-            if let Err(err) = stream.flush() {
-                print_error(err);
-                break;
-            }
-
-            drop(cam);
+        if let Err(err) = stream.write_all(image_data.as_bytes()) {
+            print_error(err);
+            break;
+        }
+        if let Err(err) = stream.write_all(&frame_buffer) {
+            print_error(err);
+            break;
+        }
+        if let Err(err) = stream.write_all(b"\r\n") {
+            print_error(err);
+            break;
+        }
+        if let Err(err) = stream.flush() {
+            print_error(err);
+            break;
         }
     }
 }
